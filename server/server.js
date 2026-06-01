8bcc00bc0e2fbb3a07b3e8ddb291201bc92ddaff
Mukund_09 <mukundamitsharma@gmail.com>
2026-06-01 17:48:41 +0530

    fix: update CORS middleware to whitelist Vercel production origin

--- server/server.js
+++ server/server.js
@@ -58,7 +58,8 @@ app.use(compression());
 const allowedOrigins = [
   'http://localhost:5173',                 
   'http://localhost:3000',                  
-  process.env.FRONTEND_URL  
+  process.env.FRONTEND_URL,
+  'https://apel-web.vercel.app'  
 ].filter(Boolean);
 app.use(cors({
   origin: function (origin, callback) {
